312d96634b756b7c7a9c6b2197aec968a6281284
aweimeow <ccps960718456@gmail.com>
2017-03-08 12:18:52 +0800

    done CheckTriangleType Function
    🚀  cmake .; make
    -- Configuring done
    -- Generating done
    -- Build files have been written to:
    /home/aweimeow/Class/SoftwareTesting/NCTU-Software-Testing-Class
    [ 20%] Built target gtest
    [ 40%] Built target gtest_main
    Scanning dependencies of target triangle
    [ 60%] Building CXX object source/CMakeFiles/triangle.dir/triangle.cpp.o
    Linking CXX shared library ../lib/libtriangle.so
    [ 60%] Built target triangle
    Linking CXX executable ../bin/_triangleTests
    [==========] Running 4 tests from 1 test case.
    [----------] Global test environment set-up.
    [----------] 4 tests from TriangleTypeTest
    [ RUN      ] TriangleTypeTest.WeakNormalTest
    [       OK ] TriangleTypeTest.WeakNormalTest (0 ms)
    [ RUN      ] TriangleTypeTest.WeakRobustTest
    [       OK ] TriangleTypeTest.WeakRobustTest (0 ms)
    [ RUN      ] TriangleTypeTest.StrongNormalTest
    [       OK ] TriangleTypeTest.StrongNormalTest (0 ms)
    [ RUN      ] TriangleTypeTest.StrongRobustTest
    [       OK ] TriangleTypeTest.StrongRobustTest (0 ms)
    [----------] 4 tests from TriangleTypeTest (0 ms total)
    
    [----------] Global test environment tear-down
    [==========] 4 tests from 1 test case ran. (0 ms total)
    [  PASSED  ] 4 tests.
    [100%] Built target _triangleTests

--- source/triangle.cpp
+++ source/triangle.cpp
@@ -1,6 +1,32 @@
+#include <math.h>
+#include <algorithm>
 #include "triangle.h"
 
 TriangleType CheckTriangleType(int a, int b, int c)
 {
-    return TRIANGLE_INVALID;
+    int arr[3] = {a, b, c};
+
+    for (const int &val : arr)
+        if (val <= 0 || val > 200)
+            return TRIANGLE_INVALID;
+
+    if (a + b <= c)
+        return TRIANGLE_INVALID;
+    if (b + c <= a)
+        return TRIANGLE_INVALID;
+    if (c + a <= b)
+        return TRIANGLE_INVALID;
+
+    if (a == b && b == c)
+        return TRIANGLE_EQUALATERAL;
+
+    std::sort(arr, arr + 3);
+    
+    if (pow(arr[0], 2) + pow(arr[1], 2) == pow(arr[2], 2))
+        return TRIANGLE_RIGHTTRIANGLE;
+
+    if (arr[0] != arr[1] && arr[1] == arr[2])
+        return TRIANGLE_ISOSCELES;
+
+    return TRIANGLE_SCALENE;
 }
